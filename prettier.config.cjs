5121ebdd98375341c75ce309fa551fc58bd4c812
shindigira <shindigira@gmail.com>
2023-05-12 15:09:53 -0400

    chore: Development Environment Setup
    * Added Vite
    * Added Tailwindcss
    * Added Eslint
    * Added Prettier
    * Implemented Absolute Pathing to @

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  printWidth: 80,
+  useTabs: false,
+  endOfLine: "auto",
+  plugins: [require("prettier-plugin-tailwindcss")],
+};
